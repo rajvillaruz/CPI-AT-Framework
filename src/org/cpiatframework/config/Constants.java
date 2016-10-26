8b6f6fab2b2c0967d9ccdae69f64c480991beb18
rajvillaruz <rochellejuanvillaruz@gmail.com>
2016-10-26 09:59:10 +0800

    Defined Configurations Class and Constants

--- src/org/cpiatframework/config/Constants.java
+++ src/org/cpiatframework/config/Constants.java
@@ -1,5 +1,15 @@
 package org.cpiatframework.config;
 
-public class Constants {
+import org.cpiatframework.util.ConfigProperties;
 
+public class Constants {
+	public static final String OBJ_PROP_FILE = Configurations.getInstance().getProperty(ConfigProperties.OBJ_PROP_FILE);
+	public static final String CHROME_DRIVER = Configurations.getInstance().getProperty(ConfigProperties.CHROME_DRIVER);
+	public static final String FIREFOX_DRIVER = Configurations.getInstance().getProperty(ConfigProperties.FIREFOX_DRIVER);
+	public static final String PATH_UPLOAD = Configurations.getInstance().getProperty(ConfigProperties.PATH_UPLOAD);
+	public static final String BROWSER_PROPERTY_CHROME = Configurations.getInstance().getProperty(ConfigProperties.BROWSER_PROPERTY_CHROME);
+	public static final String BROWSER_PROPERTY_FIREFOX= Configurations.getInstance().getProperty(ConfigProperties.BROWSER_PROPERTY_FIREFOX);
+	
+	public static final String PATH_CONFFILE = "config.properties";
+	public static final int MYCONSTANT_ONE = 1;
 }
--- src/org/cpiatframework/util/ConfigProperties.java
+++ src/org/cpiatframework/util/ConfigProperties.java
@@ -1,5 +1,10 @@
 package org.cpiatframework.util;
 
 public class ConfigProperties {
-
+	public static final String OBJ_PROP_FILE = "object.properties.file";
+	public static final String CHROME_DRIVER = "chrome.driver";
+	public static final String FIREFOX_DRIVER = "firefox.driver";
+	public static final String PATH_UPLOAD = "path.for.upload";
+	public static final String BROWSER_PROPERTY_CHROME = "browser.property.chrome";
+	public static final String BROWSER_PROPERTY_FIREFOX = "browser.property.firefox";
 }
