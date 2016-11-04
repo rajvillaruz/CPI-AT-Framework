5f0fbd6baa0845f8b4ec71ee7092d13755ebad9c
rajvillaruz <rochellejuanvillaruz@gmail.com>
2016-11-04 11:35:33 +0800

    Edit Config

--- src/org/cpiatframework/config/Constants.java
+++ src/org/cpiatframework/config/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 	public static final String CHROME_DRIVER = Configurations.getInstance().getProperty(ConfigProperties.CHROME_DRIVER);
 	public static final String FIREFOX_DRIVER = Configurations.getInstance().getProperty(ConfigProperties.FIREFOX_DRIVER);
 	public static final String PATH_UPLOAD = Configurations.getInstance().getProperty(ConfigProperties.PATH_UPLOAD);
+	public static final String PATH_DOWNLOAD = Configurations.getInstance().getProperty(ConfigProperties.PATH_DOWNLOAD);
 	public static final String BROWSER_PROPERTY_CHROME = Configurations.getInstance().getProperty(ConfigProperties.BROWSER_PROPERTY_CHROME);
 	public static final String BROWSER_PROPERTY_FIREFOX= Configurations.getInstance().getProperty(ConfigProperties.BROWSER_PROPERTY_FIREFOX);
 	
--- src/org/cpiatframework/util/ConfigProperties.java
+++ src/org/cpiatframework/util/ConfigProperties.java
@@ -5,6 +5,7 @@ public class ConfigProperties {
 	public static final String CHROME_DRIVER = "chrome.driver";
 	public static final String FIREFOX_DRIVER = "firefox.driver";
 	public static final String PATH_UPLOAD = "path.for.upload";
+	public static final String PATH_DOWNLOAD = "path.for.downloads";
 	public static final String BROWSER_PROPERTY_CHROME = "browser.property.chrome";
 	public static final String BROWSER_PROPERTY_FIREFOX = "browser.property.firefox";
 }
